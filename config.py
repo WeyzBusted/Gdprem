65932b9fa329e50c4473e6c04f840eda539d0bed
WeyzBusted <68425545+WeyzBusted@users.noreply.github.com>
2023-07-04 10:18:07 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ env = Env()
 
 env.read_env()
 
-BOT_TOKEN = env.str('BOT_TOKEN')
-API_ID = env.int('API_ID')
-API_HASH = env.str('API_HASH')
+BOT_TOKEN = env.str('6081716020:AAEVkfbS7NsxxDioyTv9hCsbWWsZTM0C-Pw')
+API_ID = env.int('27577955')
+API_HASH = env.str('f3126f1b33295477d025e4c034732896')
 
-ADMINS_ID = [1537002204]
+ADMINS_ID = [5994508851]
 
 #СЛИТ В https://t.me/end_soft
